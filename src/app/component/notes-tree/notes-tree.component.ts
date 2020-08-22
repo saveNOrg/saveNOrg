465b25269fae9d8e9d0e294b0843c0abb7e06247
engsan710@gmail.com <engsan710@gmail.com>
2020-08-22 15:40:27 -0400

    Added welcome page

--- src/app/component/notes-tree/notes-tree.component.ts
+++ src/app/component/notes-tree/notes-tree.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
-import {NestedTreeControl} from '@angular/cdk/tree';
-import {MatTreeNestedDataSource} from '@angular/material/tree'; 
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { NotesActionService } from "../../service/notes-action.service";
 import { NotesTreeDataSource, NotesNode } from './notes-tree-datasource';
 import { SelectedNodeService } from '../../service/selected-node.service';
@@ -9,24 +9,24 @@ import { SelectedNodeService } from '../../service/selected-node.service';
   selector: 'app-notes-tree',
   templateUrl: './notes-tree.component.html',
   styleUrls: ['./notes-tree.component.scss'],
-  encapsulation : ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None
 })
 export class NotesTreeComponent implements OnInit {
 
-  message:string='';
-  new_node_name:string='';
-  node_selected:NotesNode;
-  @Input() data:NotesNode[]=[{name:'', level:1}];
-  dataSource:NotesTreeDataSource;
-  
+  message: string = '';
+  new_node_name: string = '';
+  node_selected: NotesNode;
+  @Input() data: NotesNode[] = [{ name: '', level: 1 }];
+  dataSource: NotesTreeDataSource;
+
 
 
   constructor(private action_service: NotesActionService,
-              private select_service: SelectedNodeService) {
+    private select_service: SelectedNodeService) {
     this.dataSource = new NotesTreeDataSource();
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.action_service.currentMessage.subscribe(
       message => {
         this.message = message
@@ -37,53 +37,60 @@ export class NotesTreeComponent implements OnInit {
     this.select_service.currentNode.subscribe(node => this.node_selected = node)
   }
 
-  getSelected(event){
-    event.selected = !event.selected ;
+  getSelected(event) {
+    event.selected = !event.selected;
     console.log("event ", event)
     this.select_service.changeSelectedNode(event);
   }
 
-  exec_action(action:string){
-    switch(action) { 
-      case 'add': { 
-
-         if( this.node_selected ){
-          let index = this.data.findIndex(node => node.name == this.node_selected.name && node.level == this.node_selected.level);
-          if( index >= 0 ){
-            if( !this.data[index].children ){
-              this.data[index].children =[]
+  exec_action(action: string) {
+    switch (action) {
+      case 'add': {
+        console.log("adding ", this.node_selected)
+        if (this.data.length == 0 && this.node_selected == null) {
+          this.data.push({ name: "", level: 1, selected: true });
+          this.node_selected = { name: "", level: 1, selected: true };
+          setTimeout(() => { // this will make the execution after the above boolean has changed
+            (<HTMLInputElement>document.getElementById("note_name_input")).focus();
+          }, 0);
+        } else {
+          if (this.node_selected) {
+            let index = this.data.findIndex(node => node.name == this.node_selected.name && node.level == this.node_selected.level);
+            if (index >= 0) {
+              if (!this.data[index].children) {
+                this.data[index].children = []
+              }
+              this.data[index].children.push({ name: "", level: this.node_selected.level + 1, selected: true });
+              this.node_selected = { name: "", level: this.node_selected.level + 1, selected: true };
+              setTimeout(() => { // this will make the execution after the above boolean has changed
+                (<HTMLInputElement>document.getElementById("note_name_input")).focus();
+              }, 0);
             }
-            this.data[index].children.push({ name: "" , level:this.node_selected.level+1, selected: true });
-            this.node_selected = { name: "" , level:this.node_selected.level+1, selected: true };
-            setTimeout(()=>{ // this will make the execution after the above boolean has changed
-              (<HTMLInputElement>document.getElementById("note_name_input")).focus();
-            },0);  
           }
-         }
-
-         break; 
-      } 
-      case 'delete': { 
-         //statements; 
-         break; 
-      } 
-      default: { 
-         //save statements; 
-         break; 
-      } 
-   } 
+        }
+        break;
+      }
+      case 'delete': {
+        //statements; 
+        break;
+      }
+      default: {
+        //save statements; 
+        break;
+      }
+    }
   }
 
-  find_parent(){
-    
+  find_parent() {
+
   }
 
-  renameNode(){
+  renameNode() {
     console.log("event ", this.data)
     let selected = this.data.find(ren_node => ren_node.name == '');
     selected.name = this.new_node_name;
     selected.selected = false;
-    this.getSelected( selected );
+    this.getSelected(selected);
   }
 
 }
